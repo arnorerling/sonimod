a0b5c7dac0fda54f024ea14be1fe9475a7111d34
Petra Frantz <petra17@ru.is>
2017-12-04 14:29:40 +0000

    Manager breytingar

--- src/ui/MainUI.cpp
+++ src/ui/MainUI.cpp
@@ -1,4 +1,8 @@
 #include "MainUI.h"
+#include "ManagerUI.h"
+#include "SalesUI.h"
+#include "BakerUI.h"
+#include "DeliveryUI.h"
 #include <iostream>
 using namespace std;
 
@@ -9,11 +13,39 @@ MainUI::MainUI()
 
 void MainUI::start() {
 
-    cout << "Welcome to Sonimod" << endl;
-    cout << "Choose an action" << endl;
-    cout << "------------------" << endl;
-    cout << "m: Manager" << endl;
-    cout << "s: Sales" << endl;
-    cout << "b: Baker" << endl;
-    cout << "d: Delivery" << endl;
+   char select = '\0';
+
+    while (select != 'q') {
+
+        cout << "------Main Menu------" << endl;
+        cout << "m: Mananger" << endl;
+        cout << "s: Sales" << endl;
+        cout << "b: Baker" << endl;
+        cout << "d: delivery" << endl;
+        cout << "q: quit" << endl;
+        cout << "---------------------" << endl;
+
+        cin >>  select;
+
+        switch(select) {
+            case 'm': {
+                ManagerUI managerui;
+                managerui.startUI();
+                break;
+                }
+            case 's': {
+                //OrderUI orderui;
+               // orderui.startUI();
+                break;
+            }
+            case 'b': {
+                //Baker bakerui;
+                //bakerui.startUI();
+                break;
+                }
+            case 'd': {
+                cout << "Coming Soon" << endl;
+                }
+        }
+    }
 }
--- src/ui/ManagerUI.cpp
+++ src/ui/ManagerUI.cpp
@@ -1,6 +1,77 @@
 #include "ManagerUI.h"
+#include <iostream>
+using namespace std;
 
 ManagerUI::ManagerUI()
 {
     //ctor
+}
+
+void ManagerUI::startUI() {
+
+    char select = '\0';
+
+    while (select != 'q') {
+        cout << "-----Choose an action-----" << endl;
+        cout << "p: Add new pizza crust" << endl;
+        cout << "t: Add new topping" << endl;
+        cout << "m: Add new menu option" << endl;
+        cout << "d: Add new drink" << endl;
+        cout << "s: Add new sidedish" << endl;
+        cout << "b: Add new branch" << endl;
+        cout << "q: Quit" << endl;
+        cout << "--------------------------" << endl;
+
+        cin >> select;
+
+        switch(select){
+            case 'p': addCrust();
+            break;
+            case 't': addTopping();
+            break;
+            case 'm': addMenu();
+            break;
+            case 'd': addDrink();
+            break;
+            case 's': addSide();
+            break;
+            case 'b': addBranch();
+        }
+    }
+}
+
+void ManagerUI::addCrust() {
+    //Crust crust;
+    //cout << "----Add Crust----" << endl;
+    //cin >> crust;
+}
+
+void ManagerUI::addTopping() {
+    //Topping topping;
+    //cout << "----Add Topping----" << endl;
+    //cin >> topping;
+}
+
+void ManagerUI::addMenu() {
+   // Menu menu;
+    //cout << "----Add Menu----" << endl;
+    //cin >> menu;
+}
+
+void ManagerUI::addDrink() {
+    //Drink drink;
+    //cout << "----Add Drink----" << endl;
+    //cin >> drink;
+}
+
+void ManagerUI::addSide() {
+    //Sidedish sidedish;
+    //cout << "----Add Sidedish----" << endl;
+    //cin >> sidedish;;
+}
+
+void ManagerUI::addBranch() {
+    //Branch branch;
+    //cout << "----Add Branch----" << endl;
+    //cin >> branch;
 }
